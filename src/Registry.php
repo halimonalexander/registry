df166b291ae80511b4317042f949db7d141f274a
VVThanatos <vvthanatos@gmail.com>
2021-02-17 07:11:25 +0200

    codeowner email updated

--- src/Registry.php
+++ src/Registry.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of Registry.
  *
- * (c) Halimon Alexander <vvthanatos@gmail.com>
+ * (c) Halimon Alexander <a@halimon.me>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
